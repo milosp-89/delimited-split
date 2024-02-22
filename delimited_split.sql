663c97f4e41b387a695b248c872f51b7bd49d54c
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-02-22 10:39:31 +0100

    Update delimited_split.sql

--- delimited_split.sql
+++ delimited_split.sql
@@ -1,31 +1,26 @@
--- custom function to split entries/rows into the multiple rows based on values, delimited with commas
--- this is useful when one entry/row of data has multiple values stacked in one column
--- the function will create 5 rows for the same id if detected values are 5 (val1,val2,val3,val4,val5)
-
-use [xxx];
+use [db]; -- specify database name where the function will be created
 go
 
-create function [dbo].[delimitedsplit]
-        (@pstring varchar(8000), @pdelimiter char(1))
+create function [dbo].[DelimitedSplit] (@pstring varchar(8000), @pdelimiter char(1))
 returns table with schemabinding as
- return
-  with e1(n) as (
-                 select 1 union all select 1 union all select 1 union all
-                 select 1 union all select 1 union all select 1 union all
-                 select 1 union all select 1 union all select 1 union all select 1
-                ),
-       e2(n) as (select 1 from e1 a, e1 b),
-       e4(n) as (select 1 from e2 a, e2 b),
- cteTally(n) as (select top (isnull(datalength(@pstring),0)) row_number() over (order by (select null)) from e4
+        return
+                with e1(n) as (
+                         select 1 union all select 1 union all select 1 union all
+                         select 1 union all select 1 union all select 1 union all
+                         select 1 union all select 1 union all select 1 union all select 1
+                        ),
+        e2(n) as (select 1 from e1 a, e1 b),
+        e4(n) as (select 1 from e2 a, e2 b),
+cteTally(n) as (select top (isnull(datalength(@pstring),0)) row_number() over (order by (select null)) from e4
                 ),
 cteStart(n1) as (select 1 union all
                  select t.n+1 from cteTally t where substring(@pstring,t.n,1) = @pdelimiter
                 ),
 cteLen(n1,l1) as(select s.n1,
-  isnull(nullif(charindex(@pdelimiter,@pstring,s.n1),0)-s.n1,8000)
-  from cteStart s
+        isnull(nullif(charindex(@pdelimiter,@pstring,s.n1),0)-s.n1,8000)
+        from cteStart s
                 )
- select itemnumber = row_number() over(order by l.n1),
-        item       = substring(@pstring, l.n1, l.l1)
-   from cteLen l;
+select itemnumber = row_number() over(order by l.n1),
+        item = substring(@pstring, l.n1, l.l1)
+from cteLen l;
 go
