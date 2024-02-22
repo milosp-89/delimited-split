f8073ab6c349f779c3b6b3988b55dad2166524cf
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-02-22 11:06:34 +0100

    Update delimited_split.sql

--- delimited_split.sql
+++ delimited_split.sql
@@ -1,26 +1,27 @@
 use [db]; -- specify database name where the function will be created
 go
 
-create function [dbo].[DelimitedSplit] (@pstring varchar(8000), @pdelimiter char(1))
-returns table with schemabinding as
-        return
-                with e1(n) as (
-                         select 1 union all select 1 union all select 1 union all
-                         select 1 union all select 1 union all select 1 union all
-                         select 1 union all select 1 union all select 1 union all select 1
-                        ),
-        e2(n) as (select 1 from e1 a, e1 b),
-        e4(n) as (select 1 from e2 a, e2 b),
-cteTally(n) as (select top (isnull(datalength(@pstring),0)) row_number() over (order by (select null)) from e4
+create function [dbo].[DelimitedSplit]
+(@pstring varchar(8000), @pdelimiter char(1))
+        returns table with schemabinding as
+                return
+                        with e1(n) as (
+                                 select 1 union all select 1 union all select 1 union all
+                                 select 1 union all select 1 union all select 1 union all
+                                 select 1 union all select 1 union all select 1 union all select 1
+                                ),
+                             e2(n) as (select 1 from e1 a, e1 b),
+                             e4(n) as (select 1 from e2 a, e2 b),
+cte1(n) as (select top (isnull(datalength(@pstring),0)) row_number() over (order by (select null)) from e4
                 ),
-cteStart(n1) as (select 1 union all
-                 select t.n+1 from cteTally t where substring(@pstring,t.n,1) = @pdelimiter
+cte2(n1) as (select 1 union all
+                 select t.n+1 from cte1 t where substring(@pstring,t.n,1) = @pdelimiter
                 ),
-cteLen(n1,l1) as(select s.n1,
+cte3(n1,l1) as(select s.n1,
         isnull(nullif(charindex(@pdelimiter,@pstring,s.n1),0)-s.n1,8000)
-        from cteStart s
+        from cte2 s
                 )
 select itemnumber = row_number() over(order by l.n1),
         item = substring(@pstring, l.n1, l.l1)
-from cteLen l;
+from cte3 l;
 go
